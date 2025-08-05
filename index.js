4abab2e6ad082e688eae4ad83e8f08b5442460dc
brc <158456596+iambrc@users.noreply.github.com>
2025-08-05 14:39:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ const readme = `\
 
 ### My Github Contributions
 ![](https://raw.githubusercontent.com/iambrc/iambrc/main/assets/github-contribution-grid-snake.svg)
+
+<a href="https://github.com/iambrc/github-readme-stats">
+  <img height=200 align="center" src="https://github-readme-stats.vercel.app/api?username=iambrc" />
+</a>
+<a href="https://github.com/iambrc/convoychat">
+  <img height=200 align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=iambrc&layout=compact&langs_count=8&card_width=320" />
+</a>
 `
 
 console.log(readme)
